fcdc7e5f7e5e0e7406c06cb7a31e0c6db337dd0d
Chandana Nayak <nayakchandan0271@gmail.com>
2026-03-31 06:02:46 +0530

    Time: 16 ms (70.43%), Space: 24.4 MB (89.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -9,6 +9,7 @@ class Solution(object):
             if i not in dic:
                 dic[i]=1
             else:
+                dic[i]+=1
                 return True
         else:
             return False
